91f91d2de524a9a67ea390a37d9f1c0771e982f2
Anastasiya Prudnikova <Anasatasiya Prudnikova>
2022-11-20 11:38:17 -0600

    made changes

---
+++ src/access_modifiers/package1/testPackage1.java
@@ -0,0 +1,9 @@
+package access_modifiers.package1;
+
+public class testPackage1 {
+    public static void main(String[] args) {
+
+        Honda h1 = new Honda();
+        System.out.println(h1); // location
+    }
+}
---
+++ src/access_modifiers/package2/TestPackage2.java
@@ -0,0 +1,13 @@
+package access_modifiers.package2;
+
+import access_modifiers.package1.Honda;
+
+public class TestPackage2 {
+    public static void main(String[] args) {
+
+        Honda h1 = new Honda();
+        System.out.println(h1); // location
+
+
+    }
+}
